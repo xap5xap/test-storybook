9b25a5304eaf09420c8e29fdbb0a5350f6fd53f8
Xavier Perez <xap5xap@users.noreply.github.com>
2022-12-13 18:45:53 -0500

    add env

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
+
+const envVariables = {
+  MAPBOX: process.env.MAPBOX,
+};
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  env: envVariables,
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
